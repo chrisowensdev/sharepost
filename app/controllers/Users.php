11d534662e453eded9fbad4ab87d46395647c854
guitarrockstarco <guitarrockstarco@yahoo.com>
2024-04-23 17:14:25 -0400

    Finish user login/out and session setting

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -123,10 +123,28 @@
                     $data['password_err'] = 'Password must be 6 characters.';
                 }
 
+                // Check for user/email
+                if ($this->userModel->findUserByEmail($data['email'])) {
+                    // User found 
+                } else {
+                    // User not found
+                    $data['email_err'] = 'No user found';
+                }
+
                 // Make suer errors are empty
                 if(empty($data['email_err']) && empty($data['password_err'])){
                     // Validated
-                    die('Success');
+                    // Check and set logged in user
+                    $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                    
+                    if ($loggedInUser) {
+                        // Create session
+                        $this->createUserSession($loggedInUser);
+                    } else {
+                        $data['password_err'] = 'Password incorrect';
+
+                        $this->view('users/login', $data);
+                    }
                 } else {
                     // Load view with errors
                     $this->view('users/login', $data);
@@ -143,6 +161,29 @@
 
                 // Load view
                 $this->view('users/login', $data);
+            }
+        }
+
+        public function createUserSession($user) {
+            $_SESSION['user_id'] = $user->id;
+            $_SESSION['user_email'] = $user->email;
+            $_SESSION['user_name'] = $user->name;
+            redirect('pages/index');
+        }
+
+        public function logout() {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+            redirect('/users/login');
+        }
+
+        public function isLoggedIn() {
+            if (isset($_SESSION['user_id'])) {
+                return true;
+            } else {
+                return false;
             }
         }
     }
