5c9a3eca8379a523a3b85141ef323772141a8a97
guitarrockstarco <guitarrockstarco@yahoo.com>
2024-05-06 19:55:31 -0400

    Add create post functionality

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,5 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-    <div class="row">
+<?php flash('post_message'); ?>
+    <div class="row mb-3">
         <div class="col-md-6">
             <h1>Posts</h1>
         </div>
@@ -9,4 +10,16 @@
             </a>
         </div>
     </div>
+    <?php foreach($data['posts'] as $post) : ?>
+        <div class="card card-body mb-3">
+            <h4 class="card-title"><?= $post->title; ?></h4>
+            <div class="bg-light p-2 mb-3">
+                written by <?= $post->name; ?> on <?= $post->postCreated ?>
+            </div>
+            <p class="card-text">
+                <?= $post->body ?>
+                <a href="<?= URLROOT; ?>/posts/show/<?= $post->postId; ?>" class="btn btn-dark">More</a>
+            </p>
+        </div>
+    <?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
